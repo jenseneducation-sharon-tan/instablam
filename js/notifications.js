da48665aecf11426a809803069ada9a965b721f1
jenseneducation-sharon-tan <54704821+jenseneducation-sharon-tan@users.noreply.github.com>
2020-05-29 12:30:22 +0200

    cleanup folders

--- js/notifications.js
+++ js/notifications.js
@@ -4,18 +4,4 @@ function requestNotificationPermission() {
   });
 }
 
-/* function createNotification() {
-  const icon = "image/icon/apple-touch-icon.png";
-  const message = "Thank you for subscribing to our notification";
-
-  const notification = new Notification("Instablam", {
-    body: message,
-    icon: icon,
-  });
-
-  notification.addEventListener("click", (event) => {
-    window.open("http://localhost:5050/");
-  });
-}
- */
 export { requestNotificationPermission /* , createNotification */ };
